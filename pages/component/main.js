b1eabcd7b45e8f735a0e9dfdf16137e7e0679fed
burakata <hiyelata@gmail.com>
2020-10-24 16:42:18 +0300

    Update main.js

--- pages/component/main.js
+++ pages/component/main.js
@@ -5,6 +5,15 @@ function Main ({children}) {
     return (
     <div>
         <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RN5FMXVDV8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RN5FMXVDV8');
+</script>
         <title>BoxingTR</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
